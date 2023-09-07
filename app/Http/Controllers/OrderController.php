17338751db26979cf63a64e83ee991d1ea334739
julia eka <eka47577@gmail.com>
2023-09-07 07:34:10 +0700

    production true snap

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,8 +18,8 @@ class OrderController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = config('midtrans.server_key');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
+        // \Midtrans\Config::$isProduction = false;
+        // // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
         \Midtrans\Config::$is3ds = true;
